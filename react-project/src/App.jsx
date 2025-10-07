68bb687205ca53def7659e97f617785cba7606af
DilshanGDT <tharakadilshan506@gmail.com>
2025-10-07 14:34:19 +0530

    Creating a react comp - list

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -9,10 +9,35 @@ function Header( {name, year} ) {
   );
 }
 
+const items = [
+  "Macaroni and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Vegetables",
+  "Minestrone Soup"
+];
+                                    //we use () to return an object directly
+const dishObejcts = items.map((dish, idx) => ({
+  id: idx,
+  title: dish
+}));
+
+function Main({ dishes }) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+        <li key={dish.id} style={{ listStyleType: 'none'}}>{dish.title}</li>
+      ))}
+    </ul>
+  );
+}
+
 function App() {
 
   return (
+    <>
     <Header name={"Alex"} year={new Date().getFullYear()}/>
+    <Main dishes={dishObejcts}/>
+    </>
   );
 }
 
