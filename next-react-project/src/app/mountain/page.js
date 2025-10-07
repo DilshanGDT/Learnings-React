81b7fddeccb5909ef105a0a4d2d502195cd94c1d
DilshanGDT <tharakadilshan506@gmail.com>
2025-10-07 17:28:39 +0530

    fetching data

--- next-react-project/src/app/mountain/page.js
+++ next-react-project/src/app/mountain/page.js
@@ -1,3 +1,31 @@
-export default function Mountain() {
-  return <h1>Mountain Page</h1>;
+async function getData(params) {
+    const res = await fetch(`https://snowtooth-api-rest.fly.dev`);
+    return res.json();
+}
+
+
+export default async function Mountain() {
+    const data = await getData();
+
+    return (
+        <main>
+            <h1>Mountain Page</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Lift Name</th>
+                        <th>Current Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((lift) => (
+                        <tr key={lift.id}>
+                            <td>{lift.name}</td>
+                            <td>{lift.status}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </main>
+    );
 }
