46299be8afdc49ab148401b68f1edf8f8784d688
DilshanGDT <tharakadilshan506@gmail.com>
2025-10-08 09:38:32 +0530

    creating react app and react script

--- react-project-fromScratch/index.js
+++ react-project-fromScratch/index.js
@@ -1,1 +1,14 @@
-console.log('Hello, React!, this is the react rendered file');
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+// Create a React component
+const App = () => {         // (create element, props, children)
+  return React.createElement('h1', null, 'This is a React script');
+}
+
+// Render the component to the DOM
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+//root.render(<App />);   // by default JSX not supported in most browsers
+root.render(React.createElement(App));  // without JSX
