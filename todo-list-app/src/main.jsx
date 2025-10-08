921fa4493006153b8adc0b6bbf78482e36fd2df1
DilshanGDT <tharakadilshan506@gmail.com>
2025-10-08 12:05:46 +0530

    Redux setup

--- todo-list-app/src/main.jsx
+++ todo-list-app/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
 
+const store = configureStore({
+  reducer: {},
+});
+
+// provider allows us to use redux store in our app
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
